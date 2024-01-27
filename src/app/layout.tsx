e0e573d82484a8ea9aad3883f46cdb28298a67be
Abhishek V <abhishekv.dev@gmail.com>
2024-01-27 19:05:58 +0530

    Update metadata in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,20 @@ const inter = Inter({ subsets: ["latin"] });
 //directly get the items through the dropped items
 
 export const metadata: Metadata = {
-  title: "PalDex | PalWorld",
+  title: "PalDeck | PalWorld",
   description:
     "Explore the PalWorld universe and collect your favorite pals. Get information about their stats, moves, and more.",
+  generator: "PalDex",
+  manifest: "/manifest.json",
+  keywords: ["paldex", "pals", "palworld", "pokemon", "dex", "paldeck"],
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  authors: [{ name: "Abhishek V" }],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "images/types/Dark.png" },
+    { rel: "icon", url: "images/types/Dark.png" },
+  ],
 };
 
 export default function RootLayout({
