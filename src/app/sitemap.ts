be68a8c0f6a310cb1eca1c9d9fb355f80c03aacd
Abhishek V <abhishekv.dev@gmail.com>
2024-01-27 21:50:14 +0530

    Add sharp dependency and update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,14 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  console.log([
+    {
+      url: "https://my-paldeck.vercel.app/",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+  ]);
   return [
     {
       url: "https://my-paldeck.vercel.app/",
@@ -8,8 +16,8 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 1,
     },
-    ...Array(150).map((_, i) => ({
-      url: "https://my-paldeck.vercel.app/pal/" + (i + 1),
+    ...[...Array(100)].map((_, i) => ({
+      url: `https://my-paldeck.vercel.app/pal/${i + 1}`,
       lastModified: new Date(),
       changeFrequency: "monthly" as const,
       priority: 0.8,
