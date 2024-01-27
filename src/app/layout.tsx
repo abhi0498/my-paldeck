5eee683a9552b52f1d00cce4b17ee09f908dccf5
Abhishek V <abhishekv.dev@gmail.com>
2024-01-27 20:06:18 +0530

    Update next.config.mjs, package.json, globals.css, layout.tsx, and add Navbar component
    Add react-icons dependency to package.json
    
    Update tailwind.config.ts to include daisyui plugin
    
    Update yarn.lock to include culori and daisyui dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,21 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/layout/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 //directly get the items through the dropped items
 
+export const viewport: Viewport = {
+  themeColor: "black",
+  // viewPort:
+  //   "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  minimumScale: 1,
+  initialScale: 1,
+  width: "device-width",
+  userScalable: false,
+  viewportFit: "cover",
+};
 export const metadata: Metadata = {
   title: "PalDeck | PalWorld",
   description:
@@ -12,10 +23,8 @@ export const metadata: Metadata = {
   generator: "PalDex",
   manifest: "/manifest.json",
   keywords: ["paldex", "pals", "palworld", "pokemon", "dex", "paldeck"],
-  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
   authors: [{ name: "Abhishek V" }],
-  viewport:
-    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+
   icons: [
     { rel: "apple-touch-icon", url: "images/types/Dark.png" },
     { rel: "icon", url: "images/types/Dark.png" },
@@ -28,8 +37,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" data-theme="dark">
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import React from "react";
+import { FiMenu } from "react-icons/fi";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="drawer">
+          <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-content">
+            <FiMenu className="text-2xl" />
+          </div>
+          <div className="drawer-side">
+            <label
+              htmlFor="my-drawer"
+              aria-label="close sidebar"
+              className="drawer-overlay"
+            ></label>
+            <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+              {/* Sidebar content here */}
+              <li>
+                <a>Sidebar Item 1</a>
+              </li>
+              <li>
+                <a>Sidebar Item 2</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <a className="btn btn-ghost text-xl">PalDeck</a>
+      </div>
+
+      <div className="navbar-end"></div>
+    </div>
+  );
+};
+
+export default Navbar;
